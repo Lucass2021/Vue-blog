762c06f3364c61fee0cb6dddebe2947dd88ad0d8
Lucas Dias <lucas.allx@hotmail.com>
2024-01-16 19:50:15 -0300

    search bar with font awesome

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="header-container">
         <div class="title">
-            <h1><span>Code</span>lândia</h1>
+            <h1><span>Code</span>land</h1>
         </div>
         <div class="search">
-            <input type="text" placeholder="Pesquisar no blog">
+            <div>
+                <button>
+                    <font-awesome-icon class="searchIcon" :icon="['fas', 'search']"/>
+                </button>
+                <input type="text" placeholder="Search for a post">
+            </div>
         </div>
     </div>
 </template>
@@ -40,9 +45,29 @@ h1 span{
     justify-content: center;
 }
 
+.search div{
+    position: relative;
+}
+
+.search button{
+    background: none;
+    border:none;
+    padding:16px;
+    position: absolute;
+    left: 0px;
+    cursor:pointer;
+}
+
+.search .searchIcon{
+    color: #E07B67;
+    width: 16px;
+    height: 16px;
+}
+
 .search input{
     display: flex;
     padding: 16px 24px;
+    padding-left: 50px;
     align-items: center;
     gap: 16px;
     min-width: 1200px;
